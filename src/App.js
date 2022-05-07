5a1e4999b053dcb0ddc8cb02413e4eaeaf5d7530
tjxy <tjxy@tjxys-Mac-mini.local>
2022-05-07 18:23:56 +0800

    finally

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Canvas, useThree } from '@react-three/fiber'
 import { OrbitControls, TransformControls, useCursor } from '@react-three/drei'
 import { useControls, button } from 'leva'
 import create from 'zustand'
+import { Vector3 } from 'three'
 
 const useStore = create((set) => ({ target: null, setTarget: (target) => set({ target }) }))
 
@@ -46,6 +47,7 @@ const Group = forwardRef((props, ref) => {
 const Edit = () => {
   const { scene } = useThree()
   const groupRef = useRef()
+  const transFromControlRef = useRef()
   const boxRefs = useRef([])
   const [Boxes, setBoxes] = useState([])
   const [Selected, setSelected] = useState([])
@@ -54,30 +56,73 @@ const Edit = () => {
       addBox: button((get) => {
         const boxRef = createRef()
         boxRefs.current.push(boxRef)
-        setBoxes((prev) => [...prev, <Box ref={boxRef} />])
+        const boxKey = boxRefs.current.length - 1
+        setBoxes((prev) => [...prev, <Box ref={boxRef} key={boxKey} />])
       }),
       attachAllBoxToGroup: button((get) => {
-        setSelected([...Boxes])
-        setBoxes([])
+        // setSelected([...Boxes])
+        boxRefs.current.forEach((boxRef) => {
+          // console.log('boxRef', boxRef.current)
+          groupRef.current.attach(boxRef.current)
+        })
+        console.log('scene.children', scene.children)
+        // setBoxes([])
       }),
-      detachAllBoxFromGroup: button((get) => {})
+      detachAllBoxFromGroup: button((get) => {
+        groupRef.current.children.forEach((box) => {
+          const target = new Vector3(0, 0, 0)
+          console.log('box.position before', box.getWorldPosition(target))
+          box.removeFromParent()
+          scene.attach(box)
+          box.translateX(target.x)
+          box.translateY(target.y)
+          box.translateZ(target.z)
+          console.log('box.position after', box.getWorldPosition(target))
+          // box.translateOnAxis(transFromControlRef.current.worldPosition)
+        })
+        // console.log('scene.children', scene.children)
+      }),
+      log: button((get) => {
+        // if (boxRefs.current) {
+        //   boxRefs.current.forEach((box) => {
+        //     // console.log('box.current', box.current.)
+        //   })
+        // }
+        // if (transFromControlRef.current) {
+        //   console.log('transFromControlRef', transFromControlRef.current.worldPosition)
+        // }
+        console.log('scene.children', scene.children)
+      })
     },
-    [Boxes, Selected]
+    [Boxes, Selected, groupRef]
   )
-  
-  useEffect(() => {
-    console.log('Selected', Selected)
-    console.log('scene', scene.children)
-  }, [Selected])
+
+  // useEffect(() => {
+  //   console.log('Selected', Selected)
+  //   console.log('scene', scene.children)
+  // }, [Selected])
 
   // useEffect(() => {
   //   console.log('Boxes', Boxes)
+  //   console.log('scene', scene.children)
   // }, [Boxes])
+
+  // useEffect(() => {
+  //   // console.log('Boxes', Boxes)
+  //   console.log('groupRef updated, scene', scene.children)
+  // }, [groupRef.current])
+
+  // useEffect(() => {
+  //   if (transFromControlRef.current) {
+  //     console.log('transFromControlRef.current.position', transFromControlRef.current.position)
+  //   }
+  // }, [transFromControlRef.current])
 
   return (
     <>
-      <Group ref={groupRef}>{Selected}</Group>
+      <group ref={groupRef}></group>
       {Boxes}
+      {groupRef.current && <TransformControls ref={transFromControlRef} object={groupRef.current} mode={mode} />}
     </>
   )
 }
